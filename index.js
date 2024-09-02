213cc969157ed4c20473284fb57e35d8e9946f68
EllenaAmanda <ellenaamanda846@gmail.com>
2024-09-03 01:36:12 +0700

    BMI display in HTML

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 //hitung BMI
 //Rumus: Berat Badan/(Tinggi Badan/100)^2
 
+
+//get the elements
 let beratBadan = document.getElementById("weight")
 let tinggiBadan = document.getElementById("height")
 let button = document.getElementById("button")
 
+let bmiDisplay = document.getElementById("bmi")
+let bmiDesc = document.getElementById("keterangan")
+
+//fungsi rumus BMI
 function hitungBMI (BB, TB){
     let BMI = BB/(TB/100)**2
     return BMI
 }
 
 
+//ketika button ditekan
 button.addEventListener("click", (event) => {
     event.preventDefault() 
-    console.log(hitungBMI(beratBadan.value,tinggiBadan.value)) 
+    let bmiResult = hitungBMI(beratBadan.value,tinggiBadan.value).toFixed(1)
+    console.log(bmiResult) 
+
+    bmiDisplay.textContent = bmiResult
+
 })
 
